ce3679ae23aace78483a8601b86a101edea9638d
Wyatt Alt <wyatt.alt@gmail.com>
2024-03-19 04:55:56 -0700

    Adds a more comprehensive ros1msg parser test
    This fixes two bugs in the ros1msg parser and adds a more comprehensive
    set of sample data scraped from public bags.

--- util/ros1msg/transform_test.go
+++ util/ros1msg/transform_test.go
@@ -1,6 +1,9 @@
 package ros1msg_test
 
 import (
+	"io/fs"
+	"os"
+	"path/filepath"
 	"strings"
 	"testing"
 
@@ -114,152 +117,25 @@ string frame_id
 	}
 }
 
-func TestParseROS1MessageDefinition2(t *testing.T) {
-	msgdef := []byte(strings.TrimSpace(`
-#                                                                                          
-# Specified using the WGS 84 reference ellipsoid                                           
-
-# header.stamp specifies the ROS time for this measurement (the                            
-#        corresponding satellite time may be reported using the                            
-#        sensor_msgs/TimeReference message).                                               
-#                                                                                          
-# header.frame_id is the frame of reference reported by the satellite                      
-#        receiver, usually the location of the antenna.  This is a                         
-#        Euclidean frame relative to the vehicle, not a reference                          
-#        ellipsoid.                                                                        
-Header header                                                                              
-
-# satellite fix status information                                                         
-NavSatStatus status                                                                        
-
-# Latitude [degrees]. Positive is north of equator; negative is south.                     
-float64 latitude                                                                           
-
-# Longitude [degrees]. Positive is east of prime meridian; negative is west.               
-float64 longitude                                                                          
-
-# Altitude [m]. Positive is above the WGS 84 ellipsoid                                     
-# (quiet NaN if no altitude is available).                                                 
-float64 altitude                                                                           
-
-# Position covariance [m^2] defined relative to a tangential plane                         
-# through the reported position. The components are East, North, and                       
-# Up (ENU), in row-major order.                                                            
-#                                                                                          
-# Beware: this coordinate system exhibits singularities at the poles.                      
-
-float64[9] position_covariance                                                             
-
-# If the covariance of the fix is known, fill it in completely. If the                     
-# GPS receiver provides the variance of each measurement, put them                         
-# along the diagonal. If only Dilution of Precision is available,                          
-# estimate an approximate covariance from that.                                            
-
-uint8 COVARIANCE_TYPE_UNKNOWN = 0                                                          
-uint8 COVARIANCE_TYPE_APPROXIMATED = 1                                                     
-uint8 COVARIANCE_TYPE_DIAGONAL_KNOWN = 2                                                   
-uint8 COVARIANCE_TYPE_KNOWN = 3                                                            
-
-uint8 position_covariance_type                                                             
-
-================================================================================           
-MSG: std_msgs/Header                                                                       
-# Standard metadata for higher-level stamped data types.                                   
-# This is generally used to communicate timestamped data                                   
-# in a particular coordinate frame.                                                        
-#                                                                                          
-# sequence ID: consecutively increasing ID                                                 
-uint32 seq                                                                                 
-#Two-integer timestamp that is expressed as:                                               
-# * stamp.sec: seconds (stamp_secs) since epoch (in Python the variable is called 'secs')  
-# * stamp.nsec: nanoseconds since stamp_secs (in Python the variable is called 'nsecs')    
-# time-handling sugar is provided by the client library                                    
-time stamp                                                                                 
-#Frame this data is associated with                                                        
-# 0: no frame                                                                              
-# 1: global frame                                                                          
-string frame_id                                                                            
-
-================================================================================           
-MSG: sensor_msgs/NavSatStatus                                                              
-# Navigation Satellite fix status for any Global Navigation Satellite System               
-
-# Whether to output an augmented fix is determined by both the fix                         
-# type and the last time differential corrections were received.  A                        
-# fix is valid when status >= STATUS_FIX.                                                  
-
-int8 STATUS_NO_FIX =  -1        # unable to fix position                                   
-int8 STATUS_FIX =      0        # unaugmented fix                                          
-int8 STATUS_SBAS_FIX = 1        # with satellite-based augmentation                        
-int8 STATUS_GBAS_FIX = 2        # with ground-based augmentation                           
-
-int8 status                                                                                
-
-# Bits defining which Global Navigation Satellite System signals were                      
-# used by the receiver.                                                                    
-
-uint16 SERVICE_GPS =     1                                                                 
-uint16 SERVICE_GLONASS = 2                                                                 
-uint16 SERVICE_COMPASS = 4      # includes BeiDou.                                         
-uint16 SERVICE_GALILEO = 8                                                                 
-
-uint16 service
-	`))
-	schema, err := ros1msg.ParseROS1MessageDefinition("sensor_msgs", "NavSatFix", msgdef)
-	require.NoError(t, err)
-
-	require.Equal(t, "sensor_msgs/NavSatFix", schema.Name)
-	require.Len(t, schema.Fields, 7)
-}
-
-func TestParseROS1MessageDefinition(t *testing.T) {
-	msgdef := []byte(strings.TrimSpace(`
-Header header #for timestamp                                                               
-DiagnosticStatus[] status # an array of components being reported on                       
-================================================================================           
-MSG: std_msgs/Header                                                                       
-# Standard metadata for higher-level stamped data types.                                   
-# This is generally used to communicate timestamped data                                   
-# in a particular coordinate frame.                                                        
-#                                                                                          
-# sequence ID: consecutively increasing ID                                                 
-uint32 seq                                                                                 
-#Two-integer timestamp that is expressed as:                                               
-# * stamp.sec: seconds (stamp_secs) since epoch (in Python the variable is called 'secs')  
-# * stamp.nsec: nanoseconds since stamp_secs (in Python the variable is called 'nsecs')    
-# time-handling sugar is provided by the client library                                    
-time stamp                                                                                 
-#Frame this data is associated with                                                        
-# 0: no frame                                                                              
-# 1: global frame                                                                          
-string frame_id                                                                            
-
-================================================================================           
-MSG: diagnostic_msgs/DiagnosticStatus                                                      
-# This message holds the status of an individual component of the robot.                   
-#                                                                                          
-
-# Possible levels of operations                                                            
-byte OK=0                                                                                  
-byte WARN=1                                                                                
-byte ERROR=2                                                                               
-byte STALE=3                                                                               
-
-byte level # level of operation enumerated above                                           
-string name # a description of the test/component reporting                                
-string message # a description of the status                                               
-string hardware_id # a hardware unique string                                              
-KeyValue[] values # an array of values associated with the status                          
-
-
-================================================================================           
-MSG: diagnostic_msgs/KeyValue                                                              
-string key # what to label this value when viewing                                         
-string value # a value to track over time 
-	`))
-	schema, err := ros1msg.ParseROS1MessageDefinition("diagnostic_msgs", "DiagnosticArray", msgdef)
-	require.NoError(t, err)
-
-	require.Equal(t, "diagnostic_msgs/DiagnosticArray", schema.Name)
-	require.Len(t, schema.Fields, 2)
+func TestParseROS1MessageDefinitions(t *testing.T) {
+	require.NoError(t, filepath.WalkDir("testdata/schemas", func(
+		path string,
+		d fs.DirEntry,
+		err error,
+	) error {
+		if err != nil {
+			return err
+		}
+		if d.IsDir() {
+			return nil
+		}
+		bytes, err := os.ReadFile(path)
+		require.NoError(t, err)
+		parts := strings.Split(d.Name(), "-")
+		pkg := parts[0]
+		name := strings.TrimSuffix(parts[1], ".msg")
+		_, err = ros1msg.ParseROS1MessageDefinition(pkg, name, bytes)
+		require.NoError(t, err, "failed to parse %s", path)
+		return nil
+	}))
 }
