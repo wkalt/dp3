c73415c769d7f81b65160d32cb97158d157dbe94
Wyatt Alt <wyatt.alt@gmail.com>
2024-05-26 08:56:45 -0700

    Add plugin support to the CLI

--- client/dp3/util/util.go
+++ client/dp3/util/util.go
@@ -1,6 +1,7 @@
 package util
 
 import (
+	"io"
 	"os"
 )
 
@@ -10,4 +11,29 @@ func StdoutRedirected() bool {
 		return (fi.Mode() & os.ModeCharDevice) == 0
 	}
 	return false
+}
+
+// EnsureDirectoryExists creates a directory if it does not exist.
+func EnsureDirectoryExists(dir string) error {
+	if _, err := os.Stat(dir); os.IsNotExist(err) {
+		return os.MkdirAll(dir, 0755)
+	}
+	return nil
+}
+
+func Copy(src, dst string) error {
+	s, err := os.Open(src)
+	if err != nil {
+		return err
+	}
+	defer s.Close()
+
+	d, err := os.Create(dst)
+	if err != nil {
+		return err
+	}
+	defer d.Close()
+
+	_, err = io.Copy(d, s)
+	return err
 }
