f39bdf780a2615df6793489c6d4a83eda4232c35
Wyatt Alt <wyatt.alt@gmail.com>
2024-05-26 08:56:45 -0700

    Add plugin support to the CLI

---
+++ client/dp3/cmd/install.go
@@ -0,0 +1,28 @@
+package cmd
+
+import (
+	"path"
+
+	"github.com/spf13/cobra"
+	"github.com/wkalt/dp3/client/dp3/util"
+)
+
+var installCmd = &cobra.Command{
+	Use:   "install [name] [filepath]",
+	Short: "Install a plugin from a location on disk.",
+	Run: func(cmd *cobra.Command, args []string) {
+		if len(args) != 2 {
+			bailf(cmd.UsageString())
+		}
+		pluginName := args[0]
+		pluginLocation := args[1]
+		pluginDir := path.Join(configDir(), "plugins", pluginName)
+		checkErr(util.EnsureDirectoryExists(pluginDir))
+		_, filename := path.Split(pluginLocation)
+		checkErr(util.Copy(pluginLocation, path.Join(pluginDir, filename)))
+	},
+}
+
+func init() {
+	pluginCmd.AddCommand(installCmd)
+}
--- client/dp3/cmd/root.go
+++ client/dp3/cmd/root.go
@@ -3,6 +3,9 @@ package cmd
 import (
 	"fmt"
 	"os"
+	"path"
+	"path/filepath"
+	"plugin"
 
 	"github.com/spf13/cobra"
 )
@@ -26,6 +29,51 @@ func bailf(format string, args ...interface{}) {
 	os.Exit(1)
 }
 
+func checkErr(err error) {
+	if err != nil {
+		bailf("error: %v", err)
+	}
+}
+
+func configDir() string {
+	home, err := os.UserHomeDir()
+	checkErr(err)
+	return path.Join(home, ".dp3")
+}
+
+func loadPlugins() {
+	confdir := configDir()
+	plugindir := filepath.Join(confdir, "plugins")
+	// if the directory doesn't exist, there's nothing to load.
+	if _, err := os.Stat(plugindir); os.IsNotExist(err) {
+		return
+	}
+
+	checkErr(filepath.WalkDir(plugindir, func(path string, info os.DirEntry, err error) error {
+		checkErr(err)
+		if info.IsDir() {
+			return nil
+		}
+		if filepath.Ext(info.Name()) != ".so" {
+			return nil
+		}
+		plug, err := plugin.Open(path)
+		checkErr(err)
+
+		sym, err := plug.Lookup("PluginCmd")
+		checkErr(err)
+
+		cmd, ok := sym.(**cobra.Command)
+		if !ok {
+			bailf("plugin %s does not export a *cobra.Command: %T", path, sym)
+		}
+		rootCmd.AddCommand(*cmd)
+		return nil
+	}))
+
+}
+
 func init() {
+	loadPlugins()
 	rootCmd.PersistentFlags().StringVarP(&serverURL, "server-url", "", "http://localhost:8089", "server-url")
 }
